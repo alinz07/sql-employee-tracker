76cc7b640fab82b4dd6f9a4c70974d14d0f3ef75
Tony Linz <tonyllinz@gmail.com>
2022-02-27 19:42:39 -0600

    fix department list in add a role case, add update an employee functionality, finish readme

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # sql-employee-tracker
 <br/>
 
-## **Link** to video walkthrough to demonstrate app functionality: https://drive.google.com/file/d/1RqgoJq9Rtg6SPvD4Cn6zXNH8bs2z6SuA/view 
-## **Link** to project repository: https://github.com/alinz07/notetaker-mod11 
+## **Link** to video walkthrough to demonstrate app functionality: https://drive.google.com/file/d/1CR8SqyQ3coOWY7olChoBmzA_D_-VbJmx/view 
+## **Link** to project repository: https://github.com/alinz07/sql-employee-tracker 
 <br/>
 
 ## **Motivation**
-Create an application to write and save notes. Use the express package on the back end and retrieve data from a JSON file.
-
+To create a command-line app that helps manage a company's employee database using Node.js, Inquirer and MySQL.
 <br/>
 
 ## **Table of Contents**
@@ -22,47 +21,53 @@ Create an application to write and save notes. Use the express package on the ba
 <br/>
 
 ## **What Problem does this solve and how was a solution accomplished?**
-A notetake app can help organize thoughts and keep track of tasks that one needs to do. Using a server and site hosted on heroku saves money by taking advantage of their free service and saves memory from not just storing locally.
+My employee tracker app allows non-developers to easily view and intereact with information stored in a database. The app is a mix of inquirer prompts, whose returned answer objects are then used to query the database. I used an inquirer prompt to create an initial menu to ask what the user wants to do. From there a switch statement manages the flow of the app. No matter what the user chooses, they are always brought back to the main menu. 
 <br/>
 
 ## **Things I learned**
-* There are multiple ways to do a self join. I was having trouble using one way when combining with other left joins; I believe the WHERE statement was throwing off the other left joins. To solve this issue I used a left join as a self join with some help from trusty google.
-* 
+* There are multiple ways to do a self join. I was having trouble using one way when combining with other left joins; I believe the WHERE statement was throwing off the other left joins. To solve this issue I used a left join as a self join with some help from good ol' google.
+* The value of constructor functions and classes and modularization. I was short on time, otherwise I would've moved every function and query to separate files because the server.js file is CROWDED.
 <br/>
 
 ## **What makes this project stand out?**
-This project's modularization makes it stand out. I have all of my note routes in one file, scripts to run my html pages, and scripts to run the server all in different, well labeled directories, and with sensical filenames. I didn't have to add much code to the starter code to get this project running, but I did have to read a lot of documentation to make tweaks that support my file structure and desired functionality. It was a fun challenge.
+The only things that really stand out are the inquirer prompts themselves. I use a lot of statements in parens to walk the user through the questions and main menu. I can't imagine someone would use something like this professionally, but either way, I see the opportunity for improvement and the apps usefullness.
 
 <br/>
 
 ## **Challenge Criteria**
-AS A small business owner
-I WANT to be able to write and save notes
-SO THAT I can organize my thoughts and keep track of tasks I need to complete
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
 
-GIVEN a note-taking application<br/>
+GIVEN a command-line application that accepts user input<br/>
 
-* WHEN I open the Note Taker<br/>
-THEN I am presented with a landing page with a link to a notes page
+* WHEN I start the application<br/>
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
-* WHEN I click on the link to the notes page<br/>
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+* WHEN I choose to view all departments<br/>
+THEN I am presented with a formatted table showing department names and department ids
 
-* WHEN I enter a new note title and the note’s text<br/>
-THEN a Save icon appears in the navigation at the top of the page
+* WHEN I choose to view all roles<br/>
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 
-* WHEN I click on the Save icon<br/>
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+* WHEN I choose to view all employees<br/>
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
-* WHEN I click on an existing note in the list in the left-hand column<br/>
-THEN that note appears in the right-hand column
+* WHEN I choose to add a department<br/>
+THEN I am prompted to enter the name of the department and that department is added to the database
 
-* WHEN I click on the Write icon in the navigation at the top of the page<br/>
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+* WHEN I choose to add a role<br/>
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+* WHEN I choose to add an employee<br/>
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+* WHEN I choose to update an employee role<br/>
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 <br/>
 
 ## **Screenshot of Web Application**
-![gif-of-webapp](./Develop/public/assets/challenge-11.gif)
+There is a walkthrough video link at the beginning of this README instead
 <br/>
 
 ## **How to Contribute**
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-
-let deptChoicesArray = ['Operations', 'Finance', 'Legal', 'HR', 'Marketing'];
+const { query } = require('./db/connection');
 
 //start server after DB connection
 db.connect(err => {
@@ -68,7 +67,6 @@ const initResponseCheck = function (response) {
                 }
             })
             .then((deptNameObj) => {
-                deptChoicesArray.push(deptNameObj.addDept);
                 db.query(`INSERT INTO department(name)
                 VALUES ('${deptNameObj.addDept}')`, (err, rows) => {
                     if (err) {
@@ -80,60 +78,82 @@ const initResponseCheck = function (response) {
             });
             break;
         case 'add a role':
-            inquirer.prompt([
+            let deptChoicesArray = [];
 
-                {
-                    type: 'input',
-                    name: 'roleName',
-                    message: "What is the name of the role you'd like to add?",
-                    validate: roleInput => {
-                        if (roleInput) {
-                            return true;
-                        } else {
-                            return false;
+            const deptQueryCallback = function(deptArray) {
+                inquirer.prompt([
+
+                    {
+                        type: 'input',
+                        name: 'roleName',
+                        message: "What is the name of the role you'd like to add?",
+                        validate: roleInput => {
+                            if (roleInput) {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'number',
+                        name: 'salary',
+                        message: "What is this role's salary?",
+                        validate: salInput => {
+                            if (typeof salInput === 'number') {
+                                return true;
+                            } else {
+                                console.log("Please enter only numbers.")
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        type: 'checkbox',
+                        name: 'department',
+                        message: 'To which department does this role belong?',
+                        choices: deptArray,
+                        validate: roleDeptInput => {
+                            if (roleDeptInput[1]) {
+                                return false;
+                            } else {
+                                return true;
+                            }
                         }
                     }
-                },
-                {
-                    type: 'number',
-                    name: 'salary',
-                    message: "What is this role's salary?",
-                    validate: salInput => {
-                        if (typeof salInput === 'number') {
-                            return true;
-                        } else {
-                            console.log("Please enter only numbers.")
-                            return false;
-                        }
-                    }
-                },
-                {
-                    type: 'checkbox',
-                    name: 'department',
-                    message: 'To which department does this role belong?',
-                    choices: deptChoicesArray,
-                    validate: roleDeptInput => {
-                        if (roleDeptInput[1]) {
-                            return false;
-                        } else {
-                            return true;
-                        }
-                    }
-                }
-        ])
-        .then((roleAnsObj) => {
-            const {roleName, salary, department} = roleAnsObj;
-            const deptToInsert = (deptChoicesArray.indexOf(department[0])) + 1;
-            db.query(`INSERT INTO role(title, salary, department_id)
-                VALUES ('${roleName}', '${salary}', '${deptToInsert}')`, (err, rows) => {
+                ])
+                .then((roleAnsObj) => {
+                    const {roleName, salary, department} = roleAnsObj;
+                    const deptToInsert = (deptChoicesArray.indexOf(department[0])) + 1;
+                    db.query(`INSERT INTO role(title, salary, department_id)
+                        VALUES ('${roleName}', '${salary}', '${deptToInsert}')`, (err, rows) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            initApp();
+                        })
+                });
+            };
+
+            const queryDept = function() {
+
+                db.query(`SELECT department.name
+                    FROM department`, (err, rows) => {
                     if (err) {
                         console.log(err);
                         return;
-                    }
-                    initApp();
-                })
-        });
-            break;
+                    };
+                    rows.map(element => {
+                        deptChoicesArray.push(element.name);
+                    });
+                    deptQueryCallback(deptChoicesArray);
+                })                
+            };
+
+        queryDept();
+        break;
+
         case 'add an employee':
             inquirer.prompt([
 
@@ -158,7 +178,7 @@ const initResponseCheck = function (response) {
                 {
                     type: 'number',
                     name: 'roleId',
-                    message: "What is the employee's role_id? (You can run the view all roles option at the main menu to view role id's)",
+                    message: "What is the employee's role_id? (What is the employee's new role_id? (You can run the view all role id's by selecting view all roles at the main menu)",
                     validate: roleInput => {
                         if (typeof roleInput === 'number') {
                             return true;
@@ -183,7 +203,6 @@ const initResponseCheck = function (response) {
                 }
         ])
         .then((empAnsObj) => {
-            console.log(empAnsObj);
             const {firstName, lastName, roleId, manEmpId} = empAnsObj;
             db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id)
                 VALUES ('${firstName}', '${lastName}', '${roleId}', '${manEmpId}')`, (err, rows) => {
@@ -196,7 +215,74 @@ const initResponseCheck = function (response) {
         });
         break;
         case 'update an employee role':
-            console.log(7)
+            let empArray = [];
+            let empId = 0;
+
+            const queryCallback = function(empArray) {
+                inquirer.prompt([
+                    {
+                        type: 'checkbox',
+                        name: 'empToPromote',
+                        message: "Which employee's role is changing? (Please only choose one)",
+                        choices: empArray,
+                        validate: empInput => {
+                            if (empInput[1]) {
+                                return false;
+                            } else {
+                                return true;
+                            }
+                        }
+                    }
+                ])
+                .then((empToChange) => {
+                    empId = (empArray.indexOf((empToChange.empToPromote[0]).toString())) + 1;
+                    console.log(typeof empId);
+                    // this code would break if you deleted an employee because their id
+                    //in the employee table would no longer align with their index
+                    //in the empArray. You would have to do another db.query to the
+                    //employee table for id, or find a way to include the emp id in the
+                    //choices array and then access that value to run the query about
+                    //changing the employee's role that follows this then statement.
+                    inquirer.prompt(
+                        {
+                            type: 'number',
+                            name: 'roleId',
+                            message: "What is the employee's new role_id? (You can run the view all role id's by selecting view all roles at the main menu)",
+                            validate: roleInput => {
+                                if (typeof roleInput === 'number') {
+                                    return true;
+                                } else {
+                                    console.log("Only numeric values are accepted.")
+                                    return false;
+                                }
+                            }
+                        }
+                    ).then((newRoleId)=> {
+                        db.query(`UPDATE employee SET role_id = ${newRoleId.roleId} WHERE id = ${empId};`, (err, rows) => {
+                            if (err) {
+                                console.log(err)
+                            }
+                        });
+                        initApp();
+                    })
+                })
+            }
+
+            const queryEmps = function() {
+                db.query(`SELECT concat(employee.first_name, ' ', employee.last_name) as employees
+                    FROM employee`, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    };
+                    rows.map(element => {
+                        empArray.push(element.employees);
+                    });
+                    queryCallback(empArray);
+                })                
+            };
+
+            queryEmps();
             break;
         default:
             console.log("This code shouldn't be reached. debug.")
@@ -208,7 +294,7 @@ const initApp = function() {
         {
             type: 'checkbox',
             name: 'menu',
-            message: "What would you like to do? (Choose only 1)",
+            message: "What would you like to do? (Choose only 1 or press ctrl + c to quit and save)",
             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
             validate: menuInput => {
                 if (menuInput[1]) {
